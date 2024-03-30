d1a1ab42ea428e4bc72583efc5827a6aedbf3558
Brendan Blanchard <brendano257@gmail.com>
2024-03-30 06:17:54 -0400

    Add UI test for missing second arg

---
+++ tests/ui/retry_missing_should_retry.rs
@@ -0,0 +1,20 @@
+use retrys::retry;
+use retrys::ExponentialBackoffConfig;
+use std::time::Duration;
+
+const BACKOFF_CONFIG: ExponentialBackoffConfig = ExponentialBackoffConfig {
+    max_retries: 5,
+    t_wait: Duration::from_secs(1),
+    backoff: 2.0,
+    t_wait_max: None,
+    backoff_max: None,
+};
+
+fn should_retry(_: ()) -> bool {
+    true
+}
+
+#[retry(BACKOFF_CONFIG)]
+fn some_method() -> () {}
+
+fn main() {}
