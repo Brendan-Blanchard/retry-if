f925212fb9f042dcf27371ee38a88aeb1b445ae7
Brendan Blanchard <brendano257@gmail.com>
2024-03-31 08:34:04 -0400

    Add configuration docs, rename to retry-if where still needed

--- retry-if-macro/src/lib.rs
+++ retry-if-macro/src/lib.rs
@@ -4,6 +4,40 @@ use syn::parse::Parser;
 use syn::punctuated::Punctuated;
 use syn::ItemFn;
 
+/// Decorate a function with a given retry configuration.
+///
+/// Takes two arguments
+/// - `ExponentialBackoffConfig`: type defined in parent crate that configures how to back off
+/// - retry-if: a predicate that takes the same type as the output of the decorated function
+///
+/// # Example: Retrying a Result-producing Function on Err(...)
+/// The below example sets up a basic retry configuration that will retry up to five times, waiting
+/// at first 1 second, then 2 seconds, 4 seconds, etc.
+///
+/// There is no configured maxmimum time to retry across all attempts (t_wait_max), nor is there any
+/// maximum waiting time on each backoff (backoff_max).
+///
+/// ```no_run
+/// const BACKOFF_CONFIG: ExponentialBackoffConfig = ExponentialBackoffConfig {
+///     max_retries: 5,
+///     t_wait: Duration::from_secs(1),
+///     backoff: 2.0,
+///     t_wait_max: None,
+///     backoff_max: None,
+/// };
+///
+/// // this takes any argument of the same type as the output of the decorated function
+/// //  it returns a boolean specifying if the function should be retried based on the result
+/// fn retry_if(result: Result<i64, TryFromIntError>) -> bool {
+///     result.is_err()
+/// }
+///
+/// #[retry(BACKOFF_CONFIG, retry_if)]
+/// async fn fallible_call() -> Result<i64, TryFromIntError> {
+///     // this will always produce a TryFromIntError, triggering a retry
+///     i64::try_from(i128::MAX)
+/// }
+/// ```
 #[proc_macro_attribute]
 pub fn retry(
     args: proc_macro::TokenStream,
@@ -11,7 +45,7 @@ pub fn retry(
 ) -> proc_macro::TokenStream {
     let comma_punctuated = Punctuated::<Ident, syn::Token![,]>::parse_separated_nonempty;
     let punctuated_args = comma_punctuated.parse(args).expect(
-        "retry macro requires arguments of ExponentialBackoffConfig and 'should retry' function",
+        "retry macro requires arguments of ExponentialBackoffConfig and 'retry-if' function",
     );
     let mut punctuated_args_iter = punctuated_args.iter();
 
--- src/configuration.rs
+++ src/configuration.rs
@@ -3,10 +3,85 @@ use std::time::Duration;
 /// Configuration for an exponential backoff, allowing control over the entire strategy.
 ///
 /// This will retry a failing operation up to `max_tries`, waiting for a duration of `t_wait` on the
-/// first failure and `t_wait * backoff^attempt` for all remaining attempts. The maximum single wait
+/// first failure and `t_wait * backoff**attempt` for all remaining attempts. The maximum single wait
 /// can be capped by `backoff_max`, and the total waiting time before failing can be set with `t_wait_max`.
 ///
-/// TODO: example w/ series of waits
+/// The behavior of `t_wait_max` is such that the function guarantees it will not begin sleeping if
+/// sleeping would cause the function to exceed a total execution time of `t_wait_max`. It is
+/// however possible for the execution to exceed `t_wait_max` if the decorated code
+/// (e.g. calling an API) causes it to exceed this time.
+///
+///
+/// # Example: Classic Exponential Backoff
+/// This backoff configuration will retry up to 5 times, waiting 1 second at first, then 2 seconds,
+/// 4 seconds, etc.
+/// ```
+/// # use crate::retry_if::ExponentialBackoffConfig;
+/// # use tokio::time::Duration;
+///
+/// const BACKOFF_CONFIG: ExponentialBackoffConfig = ExponentialBackoffConfig {
+///     max_retries: 5,
+///     t_wait: Duration::from_secs(1),
+///     backoff: 2.0,
+///     t_wait_max: None,
+///     backoff_max: None,
+/// };
+/// ```
+///
+/// # Example: Constant Linear Retries
+/// This configuration will retry up to five times, with no exponential behavior, since the backoff
+/// exponent is `1.0`. It will wait 1 second for all retry attempts.
+/// ```
+/// # use crate::retry_if::ExponentialBackoffConfig;
+/// # use tokio::time::Duration;
+///
+/// const BACKOFF_CONFIG: ExponentialBackoffConfig = ExponentialBackoffConfig {
+///     max_retries: 5,
+///     t_wait: Duration::from_secs(1),
+///     backoff: 1.0,
+///     t_wait_max: None,
+///     backoff_max: None,
+/// };
+/// ```
+///
+/// # Example: Backoff With a Maximum Wait Time
+/// This backoff configuration will retry up to 25 times, doubling the wait with each retry.
+///
+/// Setting `t_wait_max` to 10 minutes means that regardless of the number of retries or backoff
+/// exponent, it will return in 10 minutes or less.
+///
+/// ```
+/// # use crate::retry_if::ExponentialBackoffConfig;
+/// # use tokio::time::Duration;
+///
+/// const BACKOFF_CONFIG: ExponentialBackoffConfig = ExponentialBackoffConfig {
+///     max_retries: 25,
+///     t_wait: Duration::from_secs(1),
+///     backoff: 2.0,
+///     t_wait_max: Some(Duration::from_secs(600)),
+///     backoff_max: None,
+/// };
+/// ```
+///
+/// # Example: Limited Backoff
+/// This backoff configuration will retry up to 15 times, waiting 1 second at first, then 2 seconds,
+/// 4 seconds, etc.
+///
+/// Setting `backoff_max` to 30 seconds ensures it will exponentially back off until reaching a 30s
+/// wait time, then will continue to retry every 30s until it succeeds, exhausts retries, or hits
+/// `t_wait_max` (not configured in this case).
+/// ```
+/// # use crate::retry_if::ExponentialBackoffConfig;
+/// # use tokio::time::Duration;
+///
+/// const BACKOFF_CONFIG: ExponentialBackoffConfig = ExponentialBackoffConfig {
+///     max_retries: 15,
+///     t_wait: Duration::from_secs(1),
+///     backoff: 2.0,
+///     t_wait_max: None,
+///     backoff_max: Some(Duration::from_secs(30)),
+/// };
+/// ```
 pub struct ExponentialBackoffConfig {
     pub max_retries: i32,
     pub t_wait: Duration,
